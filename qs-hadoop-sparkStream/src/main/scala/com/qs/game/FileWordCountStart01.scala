a302dd4a8f5156934a2cd7ffcec381354aadcaa9
zunzhuowei <1808862723@qq.com>
2018-06-23 12:30:50 +0800

    park streaming start

---
+++ qs-hadoop-sparkStream/src/main/scala/com/qs/game/FileWordCountStart01.scala
@@ -0,0 +1,36 @@
+package com.qs.game
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Created by zun.wei on 2018/6/23 11:41.
+  * Description: 使用Spark streaming 处理文件系统（local/hdfs）的数据
+  */
+object FileWordCountStart01 {
+
+  /*
+
+  测试，往文件夹里mv 或者 生成新的 文件（不支持嵌套目录，每次的文件格式要一样），原来的文件处理过后就不会再进入流
+
+   */
+  def main(args: Array[String]): Unit = {
+
+    //必须使用local[2]以上，因为监听流已经用去一个线程了，需要有至少一个线程（core）来执行
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("FileWordCountStart01")
+    val ssc = new StreamingContext(sparkConf,Seconds(5))
+
+    // echo aaa dd ss dfadf adfadfadsfas adfasdfasd adfas fadsf  >> aa.txt
+    // echo aaa dd ss dfadf adfadfadsfas adfasdfasd adfas fadsf  >> aa1.txt
+    val lines = ssc.textFileStream("file:///E:\\test\\ss\\")
+
+    val result = lines.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+
+    result.print()
+
+    ssc.start()
+
+    ssc.awaitTermination()
+
+  }
+}
---
+++ qs-hadoop-sparkStream/src/main/scala/com/qs/game/NetWordWordCountStart01.scala
@@ -0,0 +1,35 @@
+package com.qs.game
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Created by zun.wei on 2018/6/23 10:05.
+  * Description: 开始使用01 WordCount
+  */
+object NetWordWordCountStart01 {
+
+  /*
+   测试： nc lk 6789
+
+   */
+
+  def main(args: Array[String]): Unit = {
+    //java.io.IOException: Could not locate executable null\bin\winutils.exe in the Hadoop binaries.
+    System.setProperty("hadoop.home.dir", "E:\\hadoop")
+
+    val sparkConf = new SparkConf().setAppName("NetWordWordCountStart01").setMaster("local[*]")
+
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+
+    val lines = ssc.socketTextStream("192.168.1.187", 6789)
+    val result = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_)
+    result.print()
+
+    ssc.start()
+
+    ssc.awaitTermination()
+
+  }
+
+}
