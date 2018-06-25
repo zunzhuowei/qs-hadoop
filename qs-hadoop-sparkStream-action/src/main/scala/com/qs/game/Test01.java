ed630e19e9a91f9718ebbb6ecf60b58c9a060d18
zunzhuowei <1808862723@qq.com>
2018-06-25 15:57:43 +0800

    spark streaming flume kafka in action

---
+++ qs-hadoop-sparkStream-action/src/main/scala/com/qs/game/Test01.java
@@ -0,0 +1,9 @@
+package com.qs.game;
+
+/**
+ * Created by zun.wei on 2018/6/25 15:56.
+ * Description:
+ */
+public class Test01 {
+
+}
---
+++ qs-hadoop-sparkStream-action/src/test/scala/samples/Test01.java
@@ -0,0 +1,10 @@
+package samples;
+
+/**
+ * Created by zun.wei on 2018/6/25 15:56.
+ * Description:
+ */
+public class Test01 {
+
+
+}
