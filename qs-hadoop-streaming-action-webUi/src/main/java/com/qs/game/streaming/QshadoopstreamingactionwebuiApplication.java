57ec1d8873decefdae6bf49ad25c265ad4614713
zunzhuowei <1808862723@qq.com>
2018-06-26 23:57:52 +0800

    qs-hadoop-streaming-action-webUi init

---
+++ qs-hadoop-streaming-action-webUi/src/main/java/com/qs/game/streaming/QshadoopstreamingactionwebuiApplication.java
@@ -0,0 +1,12 @@
+package com.qs.game.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QshadoopstreamingactionwebuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QshadoopstreamingactionwebuiApplication.class, args);
+    }
+}
